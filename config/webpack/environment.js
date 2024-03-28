73f3849d6ed19e938d1149fc5cc5f3cdee2fee5a
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-03-28 23:34:11 +0500

    Before testing bootstrap 5.2.3, moving  from 4.3.1

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+// Added by Hassaaan to application understand jquery and popper.js
+const webpack = require("webpack")
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js','default']
+}))
+// End added by Hassaaan to application understand jquery and popper.js
+
 module.exports = environment
