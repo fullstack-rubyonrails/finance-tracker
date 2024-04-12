27ded23dc52732005f306f2785f2bd6f9d0a4080
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-04-12 16:22:55 +0500

    Handle response: Update display (ajax functionality) with JavaScript (Invlid & Empty Ticker)

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -12,13 +12,23 @@ class StocksController < ApplicationController
         end
         # render 'users/my_portfolio' (Remove this because "respond_to" added above)
       else
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
+        # Here for-invalid & empty stock symbol we are using same format as in IF (valid symbol) block
+        respond_to  do |format|
+          # to remove flash persistence will  falsh.now
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js {
+            render partial: 'users/result'
+          }
+        end
       end
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      respond_to  do |format|
+        # to remove flash persistence will  falsh.now
+        flash.now[:alert] = "Please enter any symbol to search"
+        format.js {
+          render partial: 'users/result'
+        }
+      end
     end
   end
-
 end
