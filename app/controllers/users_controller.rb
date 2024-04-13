9c0454ff23fc6cbd28e91fbb0c005d83636ccf23
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-04-13 16:31:55 +0500

    Create Stock instance in user_controller, call it in my_portfolio, add table view from getbootstrp websitein list partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
+    @tracked_stocks =current_user.stocks
+    # now use this  @tracked_stocks  in my  protfolio to show stock details
   end
 end
