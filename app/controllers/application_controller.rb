eb0ff4431712809e633324b18c604fb16874d575
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-04-16 15:54:52 +0500

    Navigation: in DropDown Edit User, ApplicationController: Beofe Action, FieldAdd: New.html.erb & Edit.hmtl.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Below line will pop-up login page
   before_action :authenticate_user!
   # End of Below line will pop-up login page
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
 end
