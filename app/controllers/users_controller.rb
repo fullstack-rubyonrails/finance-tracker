f76c88897049ca8612e38ec642bb94d27fa7e8b8
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-04-18 13:12:51 +0500

    Search friend AJAX submittion and JS response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,43 @@
 class UsersController < ApplicationController
-  
+
   def my_portfolio
     @tracked_stocks =current_user.stocks
     # now use this  @tracked_stocks  in my  protfolio to show stock details
   end
-  
+
   def my_friends
     @friends = current_user.friends
   end
-  
-  def search
-    render json: params[:friend]
-  end
 
+  # def search
+  #   render json: params[:friend]
+  # end
+
+# end
+
+def search
+  # convert friend to @friend instance variable to access in my_friend page and display friend's details there.
+    if params[:friend].present?  
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js {  render partial: 'users/friend_result' }
+        end
+        # render 'users/my_portfolio' (Remove this because "respond_to" added above)
+      else
+        # Here for-invalid & empty stock symbol we are using same format as in IF (valid symbol) block
+        respond_to  do |format|
+          # to remove flash persistence will  falsh.now
+          flash.now[:alert] = "Couldn't find user"
+          format.js {  render partial: 'users/friend_result' }
+        end
+      end
+    else
+      respond_to  do |format|
+        # to remove flash persistence will  falsh.now
+        flash.now[:alert] = "Please enter a friend name or email"
+        format.js {  render partial: 'users/friend_result' }
+      end
+    end
+  end
 end
